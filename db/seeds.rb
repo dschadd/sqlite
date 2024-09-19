e12bb9f8607d8184ad24054b26c48010fab34012
Daniel Schadd <dschadd@gmail.com>
2024-09-19 08:41:02 -0500

    seeds for fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,36 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Ensure the database is clean before seeding
+puts "Cleaning database..."
+User.destroy_all
+Post.destroy_all
+Address.destroy_all
+
+# Create 100 users with 100 posts each and 1 address
+puts "Creating users, posts, and addresses..."
+
+100.times do |i|
+  user = User.create!(
+    name: Faker::Name.name
+  )
+  
+  Address.create!(
+    user: user,
+    city: Faker::Address.city,
+    state: Faker::Address.state
+  )
+
+  100.times do
+    Post.create!(
+      user: user,
+      body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4)
+    )
+  end
+
+  puts "Created user #{i+1} with 100 posts and 1 address" if (i+1) % 10 == 0
+end
+
+puts "Seeding completed!"
+puts "Created #{User.count} users"
+puts "Created #{Post.count} posts"
+puts "Created #{Address.count} addresses"
